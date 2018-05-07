e7e7650de0ee443f2cff5230f9c998a0daf8ea65
Unknown <nicolaspiccitto@gmail.com>
2018-05-07 17:09:32 -0300

    TP1-V6
    Agrego animaciones para el jugador

--- Desarrollo2-TP1/Assets/Scripts/Weapon/AK47.cs
+++ Desarrollo2-TP1/Assets/Scripts/Weapon/AK47.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AK47 : MonoBehaviour {
+
+    private Animator anim;
     public float range = 100f;
-    public int bulletsPerMag = 30;
-    public int bulletsLeft;
+    [SerializeField]
+    private int bulletsPerMag = 30;
+    [SerializeField]
+    private int totalBullets = 90;
+    public int currentBullets;
 
     public Transform shootPoint;
 
     public float fireRate= 0.1f;
     float fireTimer;
 	void Start () {
-		
+
+       anim = GetComponent<Animator>();
+        currentBullets = bulletsPerMag;
+       
 	}
-	
-	
+		
 	void Update () {
         if (Input.GetButton("Fire1"))
         {
@@ -26,19 +33,28 @@ public class AK47 : MonoBehaviour {
             fireTimer += Time.deltaTime;
 	}
 
+    void FixedUpdate()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (info.IsName("Fire")) anim.SetBool("Fire", false);
+    }
+
     private void Fire()
     {
         if (fireTimer < fireRate)
         {
             return;
         }
-
+ 
         RaycastHit hit;
         if (Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name + "founf!");
+            Debug.Log(hit.transform.name + "found!");
         }
 
+        anim.SetBool("Fire", true);
+        currentBullets--;
         fireTimer = 0.0f;
 
     }
