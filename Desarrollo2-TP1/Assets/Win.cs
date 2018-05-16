30484a82f37943475a2ccead3e203d4dfa0ae320
Unknown <nicolaspiccitto@gmail.com>
2018-05-15 23:09:52 -0300

    TP1-V13
    Agrego musica, arreglo win y loose screen

---
+++ Desarrollo2-TP1/Assets/Win.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+
+    
+    private bool isActive = true;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isActive)
+            ActivarMouse();
+		
+	}
+    void ActivarMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
